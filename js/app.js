3e8160afcb594e0472a18df715b5e3c2ac1fd6ac
Suraj Ramchandran <30753067+Suraj-Ram@users.noreply.github.com>
2019-08-22 22:10:13 +0400

    Added renderTasks and addTasks and changed bg color

--- js/app.js
+++ js/app.js
@@ -1,7 +1,44 @@
 const taskInput = document.getElementById('taskInput')
 const addBtn = document.getElementById('addBtn')
 
+renderTasks()
+
+taskInput.addEventListener('keypress', e => {
+    if(e.keyCode === 13) {
+        addTask(taskInput.value)
+    }
+})
+addBtn.addEventListener('click', e  => {
+    addTask(taskInput.value)
+})
+
 function renderTasks() {
-    console.log('l')
+    tasks.forEach(task => {
+        let taskTemplate = `
+            <div class="task">
+            <div class="custom-control custom-checkbox">
+                <input type="checkbox" class="custom-control-input" id="${task.id}">
+                <label class="custom-control-label ${task.completed ? 'task-checked' : ''}" for="${task.id}">${task.title}</label>
+            </div>
+            <div class="tags">
+                
+            </div>
+            <button class="btn close">&times</button>
+            
+            </div>
+        `
+        $('.tasks').append(taskTemplate)
+    })
+}
+
+function addTask(task) {
+    console.log(`Adding: ${task}`)
+    let taskObj = {}
+    let d = new Date()
+    taskObj.id = d.getTime()
+    taskObj.title = task
+    taskObj.completed = false
+    console.log(taskObj)
+    tasks.push(taskObj)
 
 }
---
+++ js/taskStorage.js
@@ -0,0 +1,32 @@
+var tasks = [
+    {
+      "userId": 1,
+      "id": 345354,
+      "title": "delectus aut autem",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 34524267,
+      "title": "quis ut nam facilis et officia qui",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 3,
+      "title": "fugiat veniam minus",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 4,
+      "title": "et porro tempora",
+      "completed": true
+    },
+    {
+      "userId": 1,
+      "id": 5,
+      "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+      "completed": false
+    }
+  ]
